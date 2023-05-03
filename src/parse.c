59e9715fa5a051a876ed33e716900d8fdbc6c5fa
DoctorPok42 <pokdoctor@gmail.com>
2023-05-03 10:08:38 +0200

    fix separator

--- src/parse.c
+++ src/parse.c
@@ -16,24 +16,19 @@ char *check_alias(mysh_t *mysh,
 
 int check_args(parser_t *parser, mysh_t *mysh)
 {
-    if (my_strcmp(parser->cmd, "||") == 0 && mysh->status != 0) {
-        parser = parser->next;
-        return (0);
-    } else {
-        parser = parser->next->next;
-        return (0);
-    }
+    if (my_strcmp(parser->cmd, "||") == 0 && mysh->status != 0)
+        return (1);
+    if (my_strcmp(parser->cmd, "||") == 0 && mysh->status == 0)
+        return (2);
+
     if (parser->cmd[0] == '|' ||
         parser->cmd[0] == '>' ||
-        parser->cmd[0] == '<') {
-            parser = parser->next->next;
-            return (0);
-    }
-    if (parser->cmd[0] == ';') {
-        parser = parser->next;
-        return (0);
-    }
-    return (1);
+        parser->cmd[0] == '<')
+            return (2);
+
+    if (parser->cmd[0] == ';')
+        return (1);
+    return (0);
 }
 
 static int alias_input_change(mysh_t *mysh, env_t *env, char *cmd,
@@ -50,15 +45,23 @@ static int alias_input_change(mysh_t *mysh, env_t *env, char *cmd,
 int parse_input(mysh_t *mysh, env_t *env, char *cmd)
 {
     parser_t *parser = malloc(sizeof(parser_t));
+    int check_args_return = 0;
     if (parser == NULL)
         return (84);
     alias_input_change(mysh, env, cmd, parser);
     parse_args(mysh->input, parser);
     while (parser->next != NULL && mysh->status != -42 &&
         mysh->input[0] != '\n') {
-        if (check_args(parser, mysh) == 0)
+        check_args_return = check_args(parser, mysh);
+        if (check_args_return == 2) {
+            parser = parser->next->next;
             continue;
-        mysh->status = mysh_execute(mysh, env, parser); parser = parser->next;
+        } else if (check_args_return == 1) {
+            parser = parser->next;
+            continue;
+        }
+        mysh->status = mysh_execute(mysh, env, parser);
+        parser = parser->next;
     }
     return (mysh->status);
 }
