0f526a3af8c65a1a59ab262f82ddab143c286702
Crevette Bretonne <tristan.le-du@epitech.eu>
2023-05-11 14:24:05 +0200

    [FIX] valgrind but not finished

--- src/lists/parser.c
+++ src/lists/parser.c
@@ -10,12 +10,12 @@
 
 int my_strlen(char const *src);
 char **my_str_to_word_array(char const *str, char const separator);
+int my_tablen(char **tab);
 
 parser_t *put_parser(parser_t *parser, char **tab)
 {
     char **tmp = NULL;
     parser_t *head = parser;
-
     for (int i = 0; tab[i] != NULL; i++) {
         tmp = my_str_to_word_array(tab[i], ' ');
         head->cmd = malloc(sizeof(char) * (my_strlen(tmp[0]) + 1));
@@ -23,13 +23,15 @@ parser_t *put_parser(parser_t *parser, char **tab)
         for (int j = 0; tmp[0][j] != '\0'; j++) {
             head->cmd[j] = tmp[0][j];
         }
-        head->args = malloc(sizeof(char *) * (my_strlen(tab[i]) + 1));
-        head->args[my_strlen(tab[i])] = NULL;
+        head->args = malloc(sizeof(char *) * (my_tablen(tmp) + 1));
+        head->args[my_tablen(tmp)] = NULL;
         for (int j = 0; tmp[j] != NULL; j++) {
             head->args[j] = tmp[j];
         }
         head->next = malloc(sizeof(parser_t));
         head = head->next;
     }
+    parser->next->args = NULL;
+    head->next = NULL;
     return parser;
 }
