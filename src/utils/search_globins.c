fd17cdbf277b65cf7802bf2e1974ba31b88379ef
DoctorPok42 <pokdoctor@gmail.com>
2023-05-11 14:17:24 +0200

    add globins

--- src/utils/search_globins.c
+++ src/utils/search_globins.c
@@ -12,21 +12,26 @@
 
 int my_tablen(char **tab);
 
-int search_globins(parser_t *parser)
+void search_globins(parser_t *parser)
 {
-    glob_t result;
+    glob_t result = {0};
     int args_place = -1;
-
     for (int i = 1; parser->args[i] != NULL; i++) {
-        if (glob(parser->args[i], GLOB_NOCHECK |
-            GLOB_TILDE, NULL, &result) == 0) {
+        if (glob(parser->args[i], 0, NULL, &result) == 0) {
             args_place = my_tablen(parser->args);
         }
-        for (int j = 0; result.gl_pathv[j] != NULL; j++) {
-            parser->args[args_place] = strdup(result.gl_pathv[j]);
+        if (args_place == -1)
+            continue;
+        parser->args[args_place] = NULL;
+        if (result.gl_pathc < 2)
+            continue;
+        for (size_t i = 0; i < result.gl_pathc; i++) {
+            parser->args[args_place - 1] = malloc(sizeof(char) *
+            (strlen(result.gl_pathv[i]) + 1));
+            strcpy(parser->args[args_place - 1], result.gl_pathv[i]);
             args_place++;
         }
         globfree(&result);
+        parser->args[args_place] = NULL;
     }
-    return (0);
 }
